43a900345d0bd20cd1fdbab27d0cefbfb9305f95
xuejian <xuejian_qiao@163.com>
2019-09-08 15:23:53 +0800

    tijiao

---
+++ JenKinsNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenKinsNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
